f4c6e37e00730a3d5ed3f340a1e20d8e491168e9
bluestang2006 <bluestang2006@gmail.com>
2023-07-03 17:23:30 +0000

    Lab 13

---
+++ lab13.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def main():
+
+    name = input("What is your name? ")
+    day = input("What day is it today? ")
+
+    print("Hello, ", name,"! Happy ", day, "!", sep='')
+
+
+main()
