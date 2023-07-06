29e2c75c66517e1be541a6b3e4e2d21e8784fd78
bluestang2006 <bluestang2006@gmail.com>
2023-07-06 21:44:37 +0000

    Dictionary challenge simplified

--- dictionaries-challenge.py
+++ dictionaries-challenge.py
@@ -19,15 +19,22 @@ def main():
        "powers": "super strength",
        "archenemy": "adrenaline"}
                   }
-    char_name = input(f"Which character do you want to know about? (Starlord, Mystique, Hulk)\n").lower().title()           
-    while char_name not in marvelchars.keys():
-        print(f"Character unknown!")
-        char_name = input(f"Which character do you want to know about? (Starlord, Mystique, Hulk)\n").lower().title()
     
-    char_stat = input(f"What statistic do you want to know about? (real name, powers, archenemy)\n").lower()
-    while char_stat not in marvelchars.get(char_name, ''):
-        print(f"Statistic unknown!")
+    while True:
+        char_name = input(f"Which character do you want to know about? (Starlord, Mystique, Hulk)\n").lower().title()
+        if not marvelchars.get(char_name):
+            print(f"Character unknown!")
+            continue
+        else:
+            break
+
+    while True:
         char_stat = input(f"What statistic do you want to know about? (real name, powers, archenemy)\n").lower()
+        if not marvelchars.get(char_name).get(char_stat):
+            print(f"Statistic unknown!")
+            continue
+        else:
+            break
 
     if(char_stat == "real name"):
         print(f"\n{char_name}'s {char_stat} is: {marvelchars.get(char_name, '').get(char_stat, '').title()}")
