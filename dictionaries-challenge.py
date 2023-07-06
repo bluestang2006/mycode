65006f0a085b0e103b10a0fca80ff6e5569f94fc
bluestang2006 <bluestang2006@gmail.com>
2023-07-06 19:49:07 +0000

    Lab 26 Challenge - TODO: add while loops

---
+++ dictionaries-challenge.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+
+def main():
+
+    marvelchars = {
+    "Starlord":
+      {"real name": "peter quill",
+       "powers": "dance moves",
+       "archenemy": "Thanos"},
+
+    "Mystique":
+      {"real name": "raven darkholme",
+       "powers": "shape shifter",
+       "archenemy": "Professor X"},
+
+    "Hulk":
+      {"real name": "bruce banner",
+       "powers": "super strength",
+       "archenemy": "adrenaline"}
+                  }
+
+    char_name = input(f"Which character do you want to know about? (Starlord, Mystique, Hulk)\n").lower().title()
+    char_stat = input(f"What statistic do you want to know about? (real name, powers, archenemy)\n").lower()
+    
+    if(char_stat == "real name"):
+        print(f"\n{char_name}'s {char_stat} is: {marvelchars.get(char_name, '').get(char_stat, '').title()}")
+    else:
+        print(f"\n{char_name}'s {char_stat} is: {marvelchars.get(char_name, '').get(char_stat, '')}")
+
+main()
